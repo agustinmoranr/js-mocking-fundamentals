b3626628c9b254c8a9417499033e03e60d2a4b6f
Agustin Moran <agustin.moranr@gmail.com>
2024-05-06 11:06:24 -0600

    Solution to spy exercise.

--- src/__tests__/spy.js
+++ src/__tests__/spy.js
@@ -15,18 +15,17 @@ const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
 test('returns winner', () => {
-  const originalGetWinner = utils.getWinner
-  utils.getWinner = jest.fn((p1, p2) => p1)
+  const getWinnerSpy = jest.spyOn(utils, "getWinner").mockImplementation((p1, p2) => p1)
 
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
   expect(winner).toBe('Kent C. Dodds')
-  expect(utils.getWinner.mock.calls).toEqual([
+  expect(getWinnerSpy.mock.calls).toEqual([
     ['Kent C. Dodds', 'Ken Wheeler'],
     ['Kent C. Dodds', 'Ken Wheeler'],
   ])
 
   // cleanup
-  utils.getWinner = originalGetWinner
+  getWinnerSpy.mockRestore()
 })
 
 /**
--- src/no-framework/spy.js
+++ src/no-framework/spy.js
@@ -8,18 +8,24 @@ const assert = require('assert')
 const thumbWar = require('../thumb-war')
 const utils = require('../utils')
 
-function fn(impl = () => {}) {
+function fn(implementation = () => {}) {
   const mockFn = (...args) => {
     mockFn.mock.calls.push(args)
-    return impl(...args)
+    return implementation(...args)
   }
   mockFn.mock = {calls: []}
+  mockFn.mockImplementation = (newImplementation) => implementation = newImplementation
   return mockFn
+}
+
+function spyOn(object, methodName) {
+  const originalMethod = object[methodName]
+  object[methodName] = fn() //Mock del método original lo cual extiende su funcionalidad gracias a fn()
+  object[methodName].mockRestore = () => object[methodName] = originalMethod
 }
 
 spyOn(utils, 'getWinner')
 utils.getWinner.mockImplementation((p1, p2) => p1)
-
 const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
 assert.strictEqual(winner, 'Kent C. Dodds')
 assert.deepStrictEqual(utils.getWinner.mock.calls, [
@@ -32,4 +38,10 @@ utils.getWinner.mockRestore()
 
 /**
  * Checkout master branch to see the answer.
+ */
+
+/**
+ * Conclusions:
+ * 
+ * the idea behind the spyOn method is to allow the developer to mock a track an implementation and extend the functionalities to test it. And also expose methods to easily restore the original implementation. 
  */
